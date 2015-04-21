1d2d991ec9980e17aec4ac15529288b642c19e81
Margarita Vasileva <m.h.vasileva@gmail.com>
2015-04-21 15:51:01 +0300

    edit findteam, controller and views for teamadd, add state

--- app/scripts/teams/config.js
+++ app/scripts/teams/config.js
@@ -13,7 +13,25 @@
         url: '/teamfind',
         templateUrl: 'views/teams-teamfind.html',
         controller: 'TeamFindCtrl',
-        controllerAs: 'vm'
+        controllerAs: 'vm',
+        resolve: {
+          teams: teamsPrepService
+        }
       })
-    }
+      .state('teamadd', {
+        url: '/teamadd',
+        templateUrl: 'views/teams-teamadd.html',
+        controller: 'TeamAddCtrl',
+        controllerAs: 'vm',
+        resolve: {
+          skills: skillsPrepService
+        }
+      });
+  }
+  function teamsPrepService(teamservice) {
+    return teamservice.getTeams();
+  }
+  function skillsPrepService(dataservice) {
+    return dataservice.getSkills();
+  }
 })();
--- app/scripts/teams/teamservice.js
+++ app/scripts/teams/teamservice.js
@@ -10,24 +10,55 @@
 
     var service = {
       getTeams: getTeams,
+      registerTeam: registerTeam,
+      transformData: transformData
     };
 
     return service;
 
     function getTeams() {
-      return $http.get(DATA_URL + 'teams/')
-        .then(getTeamsComplete)
-        .catch(getTeamsFailed);
+       var options = { headers: { 'Authorization': 'Token ' + '25d1eb1bb5b7cfdae16a273cb1cba54e0c81e1aa' }};
+      return $http.get(DATA_URL + 'teams/', options)
+        .success(complete)
+        .error(failed);
+    };
 
-      function getTeamsComplete(response) {
-        console.log(response);
-        return response;
-      }
+    function registerTeam(data) {
+      return $http.post(DATA_URL + 'register_team/', data)
+        .success(complete)
+        .error(failed);
+    }
 
-      function getTeamsFailed(error) {
-        console.log(error);
-        return error;
-      }
+    function transformData(teams) {
+      return teams.map(function(team) {
+        team.techNames = concatenate(team.technologies_full);
+        team.leader = leader(team.members);
+        //team.id = team.members[0].teammembership_set[0].team;
+        return team;
+      });
+    }
+
+    function leader(members) {
+      return members.filter(function(member) {
+        return member.teammembership_set[0].is_leader;
+      })[0];
+    }
+    
+    function concatenate(array) {
+      var names = array.map(function(elem){
+        return elem.name;
+      }).join(', ');
+      return names;
+    };
+    
+    function complete(response) {
+      //console.log(transformData(response));
+      return transformData(response);
+    }
+
+    function failed(error) {
+      console.log(error);
+      return error;
     }
   }
 })();
