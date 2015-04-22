04a10a049e48559aba372c13e70c9349cb01c6b9
Margarita Vasileva <m.h.vasileva@gmail.com>
2015-04-22 17:00:58 +0300

    team fixes

--- app/scripts/app.module.js
+++ app/scripts/app.module.js
@@ -15,5 +15,21 @@
       'hackfmiApp.widgets',
       'hackfmiApp.core',
       'hackfmiApp.teams'
-    ]);
+    ])
+    .config(configure)
+    .run(function($rootScope, $templateCache) {
+      $rootScope.$on('$viewContentLoaded', function() {
+        $templateCache.removeAll();
+      });
+    });
+
+  function configure($stateProvider) {
+    $stateProvider
+      .state('transition', {
+        url: 'transition?destination',
+        controller: function ($state, $stateParams) {
+          $state.go($stateParams.destination);
+        }
+      });
+  }
 })();
