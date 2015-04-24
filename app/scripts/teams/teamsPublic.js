dbbb7138d243b28dd4dace0297b3bd9099866e05
Margarita Vasileva <m.h.vasileva@gmail.com>
2015-04-24 17:39:16 +0300

    add navigation to teams

--- app/scripts/teams/teamsPublic.js
+++ app/scripts/teams/teamsPublic.js
@@ -5,9 +5,10 @@
     .module('hackfmiApp.teams')
     .controller('TeamsPublicCtrl', TeamsPublicCtrl);
   
-  function TeamsPublicCtrl($state, teams) {
+  function TeamsPublicCtrl($state, teams, navbar) {
     var vm = this;
     vm.teams = teams;
+    vm.menu = navbar.anonymous();
     
     activate();
     
