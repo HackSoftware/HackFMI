e4147750514329607441e5b24064865f216ab2f5
Margarita Vasileva <m.h.vasileva@gmail.com>
2015-05-29 13:21:08 +0300

    fix login

--- app/scripts/auth/login.js
+++ app/scripts/auth/login.js
@@ -15,9 +15,13 @@
     function login() {
       return authservice.login(vm.user)
         .then(function(response) {
-          console.log(response);
           localStorage.setItem('token', response.data.auth_token);
-          $state.go('onboard');
+          if(response.status === 206) {
+            $state.go('onboard');
+          }
+          if (response.status === 200) {
+            $state.go('teamfind.notification');
+          }
         });
     }
   }
